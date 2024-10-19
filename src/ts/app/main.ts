7e6987882f4d61cdc8e3072ef4b048ead475304c
orion <oletizi@mac.com>
2024-10-19 13:53:01 -0700

    * basic web app sort of works.

--- src/ts/app/main.ts
+++ src/ts/app/main.ts
@@ -1,12 +1,15 @@
 import express from "express"
 import fs from "fs/promises"
+import path from "path"
 
 const app = express()
 const port = 3000
 
+
 app.get('/', async (req, res) => {
     try {
-        res.status(200).contentType("text/html").send((await fs.readFile('build/site/index.html')).toString())
+        // res.status(200).set('Content-Type', 'text/html').send((await fs.readFile('build/site/index.html')).toString())
+        res.sendFile(path.join(process.cwd(), 'build', 'site', 'index.html'))
     } catch (err) {
         console.error(err)
         res.status(500).send("Barf.")
@@ -15,5 +18,5 @@ app.get('/', async (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Converter app listening on port ${port}`)
 })
