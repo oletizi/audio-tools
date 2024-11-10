93008eb26eccb825d649862c90f18e3791bad5b3
orion <oletizi@mac.com>
2024-11-09 21:44:52 -0800

    * Stubbing out sysex

---
+++ src/ts/midi/sysex.ts
@@ -0,0 +1,53 @@
+/**
+ * ## Sysex message structure
+ * ```
+ * <Start of SysEx> <AKAI ID> <S5000/6000 ID> <User-selectable Device ID> <User-Refs..> ...
+ * ```
+ *
+ * Where the values of the bytes are:
+ *```
+ * <&F0> <&47> <&5E> <0..&7F> <0..&F7> ... {<240> <71> <94> <0..127> <0..247> ...}
+ *```
+ *
+ * ## Complete control message
+ * <&F0> <&47> <&5E> <0..&7F> <0..&7F> <...> <Section> <Item> <Data1> ... <DataN> <checksum> <&F7>
+ *
+ *  ## Sections
+ *  +-----------+-------------------------------+
+ *  | Section   | Description                   |
+ *  +-----------+-------------------------------+
+ *  | 0x00      | Sysex Configuration           |
+ *  | 0x02      | System Setup                  |
+ *  | 0x04      | MIDI Configuration            |
+ *  | 0x06      | Keygroup Zone Manipulation    |
+ *  | 0x08      | Keygroup Manipulation         |
+ *  | 0x0A      | Program Manipulation          |
+ *  | 0x0C      | Multi Manipulation            |
+ *  | 0x0E      | Sample Tools                  |
+ *  | 0x10      | Disk Tools                    |
+ *  | 0x12      | Multi FX Control              |
+ *  | 0x14      | Scenelist Manipulation        |
+ *  | 0x16      | MIDI Songfile Tools           |
+ *  | 0x20      | Front Panel Control           |
+ *  | 0x2A      | Alt. Program Manipulation     |
+ *  | 0x2C      | Alt. Multi Manipulation       |
+ *  | 0x2E      | Alt. Sample Tools             |
+ *  | 0x32      | Alt. Multi FX Control         |
+ *  +-----------+-------------------------------+
+ *
+ * ### Section: Keygroup Zone
+ *  +-----------+---------------+---------------+---------------------------+
+ *  | Item      | Data 1        | Data 2        | Description               |
+ *  |           | (Zone number) |               |                           |
+ *  +-----------+----------------+--------------+---------------------------+
+ *  | 0x21      | 0, 1-4        | N/A           | Get Zone Sample           |
+ *  | 0x22      | 0, 1-4        | N/A           | Get Zone Level            |
+ *  | ...       | ...           | ...           | ...                       |
+ *  +-----------+---------------+---------------+---------------------------+
+ */
+
+const START_OF_SYSEX = 0xF0
+const AKAI_ID = 0x47
+const SAMPLER_ID = 0x5E
+const DEVICE_ID = 0x00
+const USER_REFS = 0x00
