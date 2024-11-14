f17ad9214e0b365ff2b70571b9f9c72f3233a8ea
orion <oletizi@mac.com>
2024-11-13 20:06:47 -0800

    * More shoelace experiments

--- src/ts/app/App.tsx
+++ src/ts/app/App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import '@shoelace-style/shoelace/dist/themes/light.css'
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js'
 setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.18.0/cdn/')
+import SlButton from '@shoelace-style/shoelace/dist/react/button/index.js'
 function MyButton() {
     return (
         <button className={'btn btn-primary'}>I'm a button</button>
@@ -14,6 +15,7 @@ export default function App() {
         <div>
             <h1>Welcome to my app</h1>
             <MyButton />
+            <SlButton variant={'primary'}>I'm a shoelace button</SlButton>
         </div>
     )
 }
