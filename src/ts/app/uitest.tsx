bea470fd1f0e5adbc02abc4b6da46e5627b38fa1
orion <oletizi@mac.com>
2024-11-16 13:58:57 -0800

    * chakra seems to work without tailwinds

--- src/ts/app/uitest.tsx
+++ src/ts/app/uitest.tsx
@@ -1,11 +1,44 @@
 import {createRoot} from "react-dom/client";
 import * as React from "react"
-import { Provider } from "@/components/ui/provider"
+import {Provider} from "@/components/ui/provider"
+import {
+    Button,
+    createListCollection,
+    HStack,
+    SelectContent, SelectItem,
+    SelectLabel,
+    SelectRoot,
+    SelectValueText
+} from "@chakra-ui/react";
+import {SelectTrigger} from "@/components/ui/select";
 
 function App() {
+    const data = createListCollection({
+        items: [
+            {label: "React.js", value: "react"},
+            {label: "Vue.js", value: "vue"},
+            {label: "Angular", value: "angular"},
+            {label: "Svelte", value: "svelte"},
+        ]
+    })
     return (
         <Provider>
             <div>Hi</div>
+            <HStack>
+                <Button>Click me</Button>
+                <Button>Click me</Button>
+            </HStack>
+            <SelectRoot collection={data}>
+                <SelectLabel>Select a thingy</SelectLabel>
+                <SelectTrigger>
+                    <SelectValueText placeholder={'Select a thingy...'}/>
+                </SelectTrigger>
+                <SelectContent>
+                    {data.items.map((item)=>(
+                        <SelectItem item={item} key={item.value}>{item.label}</SelectItem>
+                    ))}
+                </SelectContent>
+            </SelectRoot>
         </Provider>
     )
 }
