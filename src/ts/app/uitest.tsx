7354c832de02f43c775937ede824356a3e1e07a9
orion <oletizi@mac.com>
2024-11-16 13:40:11 -0800

    * mantine isn't working well... abandoning for the time being

--- src/ts/app/uitest.tsx
+++ src/ts/app/uitest.tsx
@@ -1,5 +1,23 @@
+// core styles are required for all packages
+import '@mantine/core/styles.css';
+import { DEFAULT_THEME } from '@mantine/core';
 import {createRoot} from "react-dom/client";
 import * as React from "react"
+import {createTheme, MantineProvider} from '@mantine/core'
+import {Select} from '@mantine/core'
+
+const theme = createTheme({
+    /** Put your mantine theme override here */
+})
 
 const appRoot = createRoot(document.getElementById('app'))
-appRoot.render(<div className={'container'}>Hi</div>)
+appRoot.render(
+    <div className={'container'}>
+        <MantineProvider theme={theme}>
+            <Select
+                label="Your favorite library"
+                placeholder="Pick value"
+                data={['React', 'Angular', 'Vue', 'Svelte']}/>
+        </MantineProvider>
+    </div>
+)
