de0f9d0b6a07bc2e7b22453bfbaa0e1957dff862
orion <oletizi@mac.com>
2024-11-16 13:47:28 -0800

    * trying chakra again

--- src/ts/app/uitest.tsx
+++ src/ts/app/uitest.tsx
@@ -1,5 +1,16 @@
 import {createRoot} from "react-dom/client";
 import * as React from "react"
+import { Provider } from "@/components/ui/provider"
+
+function App() {
+    return (
+        <Provider>
+            <div>Hi</div>
+        </Provider>
+    )
+}
 
 const appRoot = createRoot(document.getElementById('app'))
-appRoot.render(<div className={'container'}>Hi</div>)
+
+
+appRoot.render(<App/>)
