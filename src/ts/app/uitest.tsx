15a7be4a406ac6cc4d904616700f8bb6c87d04d9
orion <oletizi@mac.com>
2024-11-16 13:08:38 -0800

    * ripped out chakra

--- src/ts/app/uitest.tsx
+++ src/ts/app/uitest.tsx
@@ -1,8 +1,5 @@
-import {createRoot} from "react-dom/client"
-import React from 'react'
-import { chakra } from "@chakra-ui/react"
-import { Provider } from "@/components/ui/provider"
+import {createRoot} from "react-dom/client";
+import * as React from "react"
 
 const appRoot = createRoot(document.getElementById('app'))
-
-appRoot.render(<div className={'container'}>Hi!</div>)
+appRoot.render(<div className={'container'}>Hi</div>)
