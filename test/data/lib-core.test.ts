56ae599f64c4aaa7185695ece641e2243cc53c49
orion <oletizi@mac.com>
2024-11-17 09:35:45 -0800

    * Scale function seems to work

--- test/data/lib-core.test.ts
+++ test/data/lib-core.test.ts
@@ -4,5 +4,9 @@ import {expect} from "chai";
 describe('Core lib', () => {
     it('scale', () => {
         expect(scale(50, 0, 100, 0, 10)).eq(5)
+        expect(scale(-1, -100, 100, 0, 200)).eq(99)
+    })
+    it('positive slider', () => {
+
     })
 })
