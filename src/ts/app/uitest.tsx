4ee2b64b2140cd5c6bb1445b55212d20f5615af6
orion <oletizi@mac.com>
2024-11-16 16:52:40 -0800

    * More chakra experiments

--- src/ts/app/uitest.tsx
+++ src/ts/app/uitest.tsx
@@ -2,15 +2,30 @@ import {createRoot} from "react-dom/client";
 import * as React from "react"
 import {Provider} from "@/components/ui/provider"
 import {
-    Button,
-    createListCollection,
-    HStack,
+    Button, Card, Container,
+    createListCollection, Field, Flex, HStack,
     SelectContent, SelectItem,
     SelectLabel,
     SelectRoot,
     SelectValueText
 } from "@chakra-ui/react";
 import {SelectTrigger} from "@/components/ui/select";
+
+function Selector({data}) {
+    return (
+        <SelectRoot collection={data}>
+            <SelectLabel>Select a thingy</SelectLabel>
+            <SelectTrigger>
+                <SelectValueText placeholder={'Select a thingy...'}/>
+            </SelectTrigger>
+            <SelectContent>
+                {data.items.map((item) => (
+                    <SelectItem item={item} key={item.value}>{item.label}</SelectItem>
+                ))}
+            </SelectContent>
+        </SelectRoot>
+    )
+}
 
 function App() {
     const data = createListCollection({
@@ -23,22 +38,18 @@ function App() {
     })
     return (
         <Provider>
-            <div>Hi</div>
-            <HStack>
-                <Button>Click me</Button>
-                <Button>Click me</Button>
-            </HStack>
-            <SelectRoot collection={data}>
-                <SelectLabel>Select a thingy</SelectLabel>
-                <SelectTrigger>
-                    <SelectValueText placeholder={'Select a thingy...'}/>
-                </SelectTrigger>
-                <SelectContent>
-                    {data.items.map((item)=>(
-                        <SelectItem item={item} key={item.value}>{item.label}</SelectItem>
-                    ))}
-                </SelectContent>
-            </SelectRoot>
+            <Container>
+                <Flex align={'center'} gap={3}>
+                    <Card.Root>
+                        <Card.Body>
+                            <Card.Description>
+                                Hi. This is a nice thing in a nice card.
+                            </Card.Description>
+                        </Card.Body>
+                    </Card.Root>
+                    <Selector data={data}/>
+                </Flex>
+            </Container>
         </Provider>
     )
 }
