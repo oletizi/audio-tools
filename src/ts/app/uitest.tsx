5ad0e9170ddfc6883b17ac34b017960d43119122
orion <oletizi@mac.com>
2024-11-16 17:56:29 -0800

    * More chakra experiments

--- src/ts/app/uitest.tsx
+++ src/ts/app/uitest.tsx
@@ -2,12 +2,13 @@ import {createRoot} from "react-dom/client";
 import * as React from "react"
 import {Provider} from "@/components/ui/provider"
 import {
+    Box,
     Button, Card, Container,
     createListCollection, Field, Flex, HStack,
     SelectContent, SelectItem,
     SelectLabel,
     SelectRoot,
-    SelectValueText, Stack
+    SelectValueText, Stack, Tabs
 } from "@chakra-ui/react";
 import {SelectTrigger} from "@/components/ui/select";
 
@@ -24,6 +25,27 @@ function Selector({data}) {
                 ))}
             </SelectContent>
         </SelectRoot>
+    )
+}
+
+function Tabber() {
+    return (
+        <Tabs.Root defaultValue="thing-3">
+            <Tabs.List>
+                <Tabs.Trigger value="thing-1">Thing 1</Tabs.Trigger>
+                <Tabs.Trigger value="thing-2">Thing 2</Tabs.Trigger>
+                <Tabs.Trigger value="thing-3">Thing 3</Tabs.Trigger>
+            </Tabs.List>
+            <Tabs.Content value="thing-1">Tab content for thing 1.</Tabs.Content>
+            <Tabs.Content value="thing-2">Tab content for thing 2.</Tabs.Content>
+            <Tabs.Content value="thing-3">
+                <Card.Root>
+                    <Card.Body>
+                        Tab content for thing 3.
+                    </Card.Body>
+                </Card.Root>
+            </Tabs.Content>
+        </Tabs.Root>
     )
 }
 
@@ -54,11 +76,18 @@ function App() {
                                 <Button>Click me, too.</Button>
                             </Stack>
                             <Card.Description>
-                                This is a nice description. It must be text, though. It's inside a p tag
+                                This is a nice description. It must be text, though.
                             </Card.Description>
                         </Card.Body>
                     </Card.Root>
-                    <Selector data={data}/>
+                    <Card.Root>
+                        <Card.Body>
+                            <Selector data={data}/>
+                        </Card.Body>
+                    </Card.Root>
+                    <Box flexGrow={1}>
+                        <Tabber/>
+                    </Box>
                 </Flex>
             </Container>
         </Provider>
