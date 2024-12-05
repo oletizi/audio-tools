6bfbc07b17fb230490a4e953137f82e36750272b
orion <oletizi@mac.com>
2024-12-04 22:19:23 -0800

    * Converting API to next

--- src/app/translate/page.tsx
+++ src/app/translate/page.tsx
@@ -1,5 +1,8 @@
 import {FileList} from "@/components/file-list"
+import {newClientCommon} from "@/client/client-common";
+const client = newClientCommon((msg) => console.log(msg), (msg) => console.error(msg))
 export default function Translate() {
+    const config = client.fetchConfig()
     return (<div className="container mx-auto">
         <FileList/>
     </div>)
--- src/components/file-list.tsx
+++ src/components/file-list.tsx
@@ -1,3 +1,10 @@
-export function FileList() {
-    return (<div>I'm a file list</div>)
+import List from '@mui/material/list'
+import {ListItem} from "@mui/material";
+
+export function FileList({data}) {
+
+    return (<List>
+        <ListItem>I'm a nice list item.</ListItem>
+        <ListItem>I'm another nice list item.</ListItem>
+    </List>)
 }
