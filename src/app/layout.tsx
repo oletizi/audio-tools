61eb7b8ca3c01b71f9d93feb54385ed604c55a43
orion <oletizi@mac.com>
2024-12-04 21:43:07 -0800

    * Adding next & material icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import {Metadata} from "next";
+import {AppRouterCacheProvider} from '@mui/material-nextjs/v15-appRouter';
 import "./globals.css";
+import {Roboto} from 'next/font/google';
+import {ThemeProvider} from '@mui/material/styles';
+import theme from '../theme'
+
+const roboto = Roboto({
+    weight: ['300', '400', '500', '700'],
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-roboto',
+})
+
 export const metadata: Metadata = {
     title: 'Akai Sampler App',
     description: 'A set of experimental tools for the Akai S5000/S6000 series samplers.',
@@ -13,8 +25,12 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-        <body>
-        <div id="root">{children}</div>
+        <body className={roboto.variable}>
+        <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>
+                <div id="root">{children}</div>
+            </ThemeProvider>
+        </AppRouterCacheProvider>
         </body>
         </html>
     )
---
+++ src/app/translate/page.tsx
@@ -0,0 +1,3 @@
+export default function Translate() {
+    return (<div>Translate page.</div>)
+}
