73363a063b18900d54066ce3054139c730131b2c
orion <oletizi@mac.com>
2024-12-23 13:40:45 -0800

    * refining knob min/max

--- src/app/canvas/page.tsx
+++ src/app/canvas/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 import {Knob} from "@/components/knob";
 import {useState} from "react";
+import {Stack} from "@mui/material";
 
 export default function Page() {
+    const mainColor = "#aaaaaa"
     const [value, setValue] = useState(0)
-    return <div className="container">
-        <Knob color="#666" backgroundColor="#aaa" strokeWidth={3} onChange={(v) => setValue(v)} min={0} max={127}/>
-        <div>{Math.round(value)}</div>
+    return <div className="container pt-10">
+        <Stack className="flex flex-col items-center">
+            <Knob color="#fff" backgroundColor={mainColor} strokeWidth={3} onChange={(v) => setValue(v)} min={0} max={127}/>
+            <div className="text-gray-400">{Math.round(value)}</div>
+        </Stack>
     </div>
 }
 
