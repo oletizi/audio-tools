5b32ec2be4bedff85da967d048df587378980920
orion <oletizi@mac.com>
2025-01-15 22:50:22 -0800

    S3000XL * adding support for code generation for akai s3000xl

--- src/midi/devices/s3000xl.ts
+++ src/midi/devices/s3000xl.ts
@@ -1,5 +1,5 @@
 //
-// GENERATED Wed Jan 15 2025 22:44:18 GMT-0800 (Pacific Standard Time). DO NOT EDIT.
+// GENERATED Wed Jan 15 2025 22:49:11 GMT-0800 (Pacific Standard Time). DO NOT EDIT.
 //    
 import {byte2nibblesLE, bytes2numberLE, nibbles2byte} from "@/lib/lib-core"
 import {newClientOutput} from "@/lib/process-output"
@@ -8,8 +8,11 @@ import {nextByte, akaiByte2String} from "@/midi/akai-s3000xl"
 export interface ProgramHeader {
   KGRP1: number    // Block address of first keygroup (internal use)
   PRNAME: string    // Name of program
-  PRGNUM: number    // MIDI program number
-  PMCHAN: number    // MIDI channel
+  PRGNUM: number    // MIDI program number; Range: 0 to 128; After sending data to this parameter, Miscellaneous function BTSORT should be triggered to resort the list of programs into order and to flag active programs.
+  PMCHAN: number    // MIDI channel; Range: 255 signifies OMNI, 0 to 15 indicate MIDI channel
+  POLYPH: number    // Depth of polyphony; Range: 0 to 31 (these represent polyphony values of 1 to 32)
+  PRIORT: number    // Priority of voices playing this program; Range: 0=low, 1=norm, 2=high, 3=hold
+  PLAYLO: number    // Lower limit of play range; Range: 21 to 127 represents A1 to G8
 }
 
 export function parseProgramHeader(data: number[], o: ProgramHeader) {
@@ -40,7 +43,7 @@ export function parseProgramHeader(data: number[], o: ProgramHeader) {
           o.PRNAME += akaiByte2String([v.value])
     }
 
-    // MIDI program number
+    // MIDI program number; Range: 0 to 128; After sending data to this parameter, Miscellaneous function BTSORT should be triggered to resort the list of programs into order and to flag active programs.
     out.log('PRGNUM: offset: ' + reloff())
     b = []
     for (let i=0; i<1; i++) {
@@ -48,12 +51,36 @@ export function parseProgramHeader(data: number[], o: ProgramHeader) {
     }
     o.PRGNUM = bytes2numberLE(b)
 
-    // MIDI channel
+    // MIDI channel; Range: 255 signifies OMNI, 0 to 15 indicate MIDI channel
     out.log('PMCHAN: offset: ' + reloff())
     b = []
     for (let i=0; i<1; i++) {
         b.push(nextByte(data, v).value)
     }
     o.PMCHAN = bytes2numberLE(b)
+
+    // Depth of polyphony; Range: 0 to 31 (these represent polyphony values of 1 to 32)
+    out.log('POLYPH: offset: ' + reloff())
+    b = []
+    for (let i=0; i<1; i++) {
+        b.push(nextByte(data, v).value)
+    }
+    o.POLYPH = bytes2numberLE(b)
+
+    // Priority of voices playing this program; Range: 0=low, 1=norm, 2=high, 3=hold
+    out.log('PRIORT: offset: ' + reloff())
+    b = []
+    for (let i=0; i<1; i++) {
+        b.push(nextByte(data, v).value)
+    }
+    o.PRIORT = bytes2numberLE(b)
+
+    // Lower limit of play range; Range: 21 to 127 represents A1 to G8
+    out.log('PLAYLO: offset: ' + reloff())
+    b = []
+    for (let i=0; i<1; i++) {
+        b.push(nextByte(data, v).value)
+    }
+    o.PLAYLO = bytes2numberLE(b)
 
 }
