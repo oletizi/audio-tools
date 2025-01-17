3ac968d8e3bb577bca6009f0f65e2a774d9afcba
orion <oletizi@mac.com>
2025-01-16 19:06:16 -0800

    * starting a cli for s3000xl

--- src/cli/cli-s3000xl.tsx
+++ src/cli/cli-s3000xl.tsx
@@ -1,14 +1,31 @@
 import React, {useState} from 'react';
 import {render, Box, Text, useApp, useInput, useStdout} from 'ink';
+import {Select} from '@inkjs/ui'
+import midi from "midi";
+import {newDevice} from "@/midi/akai-s3000xl.js";
 
-
+const midiInput = new midi.Input()
+const midiOutput = new midi.Output()
+const device = newDevice(midiInput, midiOutput)
 
 function Button(props) {
     return (<Box borderStyle="single" paddingLeft={1} paddingRight={1}><Text>{props.children}</Text></Box>)
 }
 
 function StartScreen() {
-    return (<Text>Hello!</Text>)
+
+    return (<Box>
+        <Text>MIDI Input</Text>
+        <MidiSelect midiHandle={midiInput}/>
+    </Box>)
+}
+
+function MidiSelect({midiHandle}: {midiHandle: midi.Input | midi.Output}) {
+    const options = []
+    for (let i=0; i< midiHandle.getPortCount(); i++) {
+        options.push({label: midiHandle.getPortName(i), value: i})
+    }
+    return(<Select options={options}/>)
 }
 
 function Thingy() {
