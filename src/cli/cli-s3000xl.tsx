b73773d1385d142425d7f9d1d01510f35f5624a1
orion <oletizi@mac.com>
2025-01-16 18:33:18 -0800

    * starting a cli for s3000xl

--- src/cli/cli-s3000xl.tsx
+++ src/cli/cli-s3000xl.tsx
@@ -1,17 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import {render, Box, Text, useInput} from 'ink';
+import React, {useState} from 'react';
+import {render, Box, Text, useApp, useInput, useStdout} from 'ink';
+
+
+
+function Button(props) {
+    return (<Box borderStyle="single" paddingLeft={1} paddingRight={1}><Text>{props.children}</Text></Box>)
+}
+
+function StartScreen() {
+    return (<Text>Hello!</Text>)
+}
 
 function Thingy() {
+    const {exit} = useApp();
+    const {stdout} = useStdout()
+    const [screen, setScreen] = useState(<StartScreen/>)
     const [msg, setMessage] = useState('Display here?')
-    useInput((input, key) => {
-        setMessage(msg + input)
+    useInput((input: string, key) => {
+        if (input.toUpperCase() === 'Q') {
+            setMessage('You quit')
+            exit()
+        } else if (input.toUpperCase() === 'M') {
+            setMessage('Do MIDI!')
+        } else {
+            setMessage(input)
+        }
     })
     return (
         <>
-            <Box borderStyle='single' padding='1'>
-                <Text>{msg}</Text>
+            <Box borderStyle='single' padding='1' height={stdout.rows - 4}>{screen}</Box>
+            <Box>
+                <Button>M: MIDI</Button>
+                <Button>Q: Quit</Button>
             </Box>
-            <Box paddingLeft={2} paddingRight={2}><Text>Commands here?</Text></Box>
         </>
     )
 }
