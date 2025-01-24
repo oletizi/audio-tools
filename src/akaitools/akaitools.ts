65bf09862dd8f1871d5b859ca1079799e2c91dd2
orion <oletizi@mac.com>
2025-01-23 17:57:48 -0800

    * Akaitools: adding support for akailist

--- src/akaitools/akaitools.ts
+++ src/akaitools/akaitools.ts
@@ -1,8 +1,85 @@
 import fs from "fs/promises";
+import {spawn} from 'child_process'
+import path from "path";
+import {Result} from "@/lib/lib-core.js";
 
 export interface AkaiToolsConfig {
     diskFile: string
     akaiToolsPath: string
+}
+
+export enum AkaiRecordType {
+    NULL = 'NULL',
+    VOLUME = 'S3000 VOLUME',
+    PROGRAM = 'S3000 PROGRAM',
+    SAMPLE = 'S3000 SAMPLE'
+}
+
+export interface AkaiRecord {
+    parent: string
+    type: AkaiRecordType
+    name: string
+    block: number
+    size: number
+}
+
+export interface AkaiRecordResult extends Result {
+    data: AkaiRecord[]
+}
+
+export async function akaiList(c: AkaiToolsConfig, akaiPath: string = '/', partition = 1) {
+    await validateConfig(c)
+    const bin = path.join(c.akaiToolsPath, 'akailist')
+    const args = ['-f', `${c.diskFile}`, '-l', '-p', String(partition), '-u', `"${akaiPath}"`]
+    process.env['PERL5LIB'] = c.akaiToolsPath
+
+    return new Promise<AkaiRecordResult>(async (resolve, reject) => {
+        const rv: AkaiRecordResult = {data: [], errors: []}
+        const child = spawn(bin, args, {shell: true})
+        // child.stdout.pipe(process.stdout)
+
+        let record: AkaiRecord = {block: 0, name: "", parent: "", size: 0, type: AkaiRecordType.NULL}
+        let parsing = false
+        child.stdout.setEncoding('utf8')
+        child.stdout.on('data', (data) => {
+            for (const line of String(data).split('\n')) {
+                if (data.startsWith('/') && data.endsWith(':')) {
+                    if (parsing) {
+                        rv.data.push(record)
+                        record = {block: 0, name: "", parent: "", size: 0, type: AkaiRecordType.NULL}
+                    }
+                    parsing = true
+                } else {
+                    if (line === '') {
+                        continue
+                    }
+                    record.type = line.slice(0, 23).trim() as AkaiRecordType
+                    record.block = Number.parseInt(line.slice(23, 25).trim())
+                    record.size = Number.parseInt(line.slice(25, 34).trim())
+                    record.name = line.slice(35).trim()
+                    rv.data.push(record)
+                }
+            }
+        })
+
+        child.on('error', (e) => {
+            rv.errors.push(e);
+            resolve(rv)
+        })
+        child.on('close', (code, signal) => {
+            console.log(`Process exited with code ${code} and signal ${signal}`)
+            if (code !== null) {
+                resolve(rv)
+            } else {
+                reject(new Error('Process terminated without an exit code.'))
+            }
+        })
+        child.stderr.on('data', data => {
+            rv.errors.push(new Error(data))
+        })
+
+        setTimeout(() => reject(new Error(`Timout executing ${bin}.`)), 5 * 1000)
+    })
 }
 
 export async function validateConfig(c: AkaiToolsConfig) {
@@ -17,7 +94,7 @@ export async function validateConfig(c: AkaiToolsConfig) {
         throw new Error(`Akai tools path is not a directory: ${c.akaiToolsPath}`)
     }
 
-    return new Set([
+    if (!new Set([
         'akai2wav',
         'akailist',
         'akaiconv',
@@ -28,7 +105,7 @@ export async function validateConfig(c: AkaiToolsConfig) {
         'akaiwrite',
         'any2akai',
         'akai2wav'
-    ]).isSubsetOf(new Set(await fs.readdir(c.akaiToolsPath)))
+    ]).isSubsetOf(new Set(await fs.readdir(c.akaiToolsPath)))) {
+        throw new Error(`Akai tools path does not contain expected executables.`)
+    }
 }
-
-
--- test/integration/akaitools.test.ts
+++ test/integration/akaitools.test.ts
@@ -1,14 +1,26 @@
-import {AkaiToolsConfig, validateConfig} from "../../src/akaitools/akaitools";
+import {akaiList, validateConfig} from "../../src/akaitools/akaitools";
 import path from "path";
 import {expect} from "chai";
-import fs from "fs/promises";
 
 describe('Test interaction w/ akaitools and akai files.', async () => {
-    it('Validates config', async () => {
-        const config: AkaiToolsConfig = {
+    function newConfig() {
+        return {
             akaiToolsPath: path.join('..', 'akaitools-1.5'),
             diskFile: path.join('test', 'data', 's3000xl', 'akai.img')
         }
-        expect(await validateConfig(config)).true
+    }
+
+    it('Validates config', async () => {
+        expect(await validateConfig(newConfig()))
+    })
+
+    it(`Lists an Akai disk image`, async function () {
+        this.timeout(12 * 1000)
+        let result = await akaiList(newConfig(), 'VOLUME 1')
+        for (const e of result.errors) {
+            console.error(e)
+        }
+        expect(result.errors.length).eq(0)
+        expect(result.data.length).eq(5)
     })
 })
