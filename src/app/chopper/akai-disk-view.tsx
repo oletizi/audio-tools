a4d4324241fb03f8e536b161fe434bb78fcd54ac
orion <oletizi@mac.com>
2025-01-30 21:00:19 -0800

    Chopper * Creating Akai disk screen

--- src/app/chopper/akai-disk-view.tsx
+++ src/app/chopper/akai-disk-view.tsx
@@ -17,22 +17,22 @@ import RefreshIcon from '@mui/icons-material/Refresh'
 import StorageIcon from '@mui/icons-material/Storage';
 import SaveIcon from '@mui/icons-material/Save';
 import path from "path";
-import {AudioFile} from "@mui/icons-material";
+import {AudioFile, ExpandLess, ExpandMore} from "@mui/icons-material";
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import dayjs from "dayjs";
 
 export function AkaiDiskView({data, update}: { data: AkaiDisk, update: () => AkaiDisk }) {
     const [disk, setDisk] = useState(data)
-    const [open, setOpen] = useState(true)
-    const dateFormat = dayjs(new Date(disk.timestamp)).format()
+    let items = 0
     return (
         <Card>
-            <CardHeader title="Your Disk So Far"/>
+            <CardHeader className="shadow-md" title="Your Disk So Far"/>
             <CardContent>
                 {disk.partitions.map(partition => {
+                    items++
                     return (
                         <List key={partition.name} sx={{width: '100%'}}>
-                            <PartitionView data={partition}/>
+                            <PartitionView data={partition} openDefault={items === 1}/>
                         </List>)
                 })}
             </CardContent>
@@ -45,17 +45,20 @@ export function AkaiDiskView({data, update}: { data: AkaiDisk, update: () => Aka
     )
 }
 
-function PartitionView({data}: { data: AkaiPartition }) {
-    const [open, setOpen] = useState(true)
+function PartitionView({data, openDefault}: { data: AkaiPartition, openDefault: boolean }) {
+    const [open, setOpen] = useState(openDefault)
+    let items = 0
     return (<>
             <ListItemButton onClick={() => setOpen(!open)}>
                 <ListItemIcon><StorageIcon/></ListItemIcon>
                 <ListItemText primary={`Partition ${data.name}`}/>
+                {open ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
             <Collapse in={open}>
                 <List sx={{width: '100%'}} dense={true} disablePadding>
                     {data.volumes.map(volume => {
-                        return (<VolumeView key={volume.name} data={volume}/>)
+                        items++
+                        return (<VolumeView key={volume.name}  openDefault={items === 1}  data={volume}/>)
                     })}
                 </List>
             </Collapse>
@@ -63,16 +66,19 @@ function PartitionView({data}: { data: AkaiPartition }) {
     )
 }
 
-function VolumeView({data}: { data: AkaiVolume }) {
-    const [open, setOpen] = useState(true)
+function VolumeView({data, openDefault}: { data: AkaiVolume, openDefault: boolean}) {
+    const [open, setOpen] = useState(openDefault)
+    let items = 0
     return (<>
             <ListItemButton onClick={() => setOpen(!open)}>
                 <ListItemIcon><SaveIcon/></ListItemIcon>
                 <ListItemText primary={`Vol: ${path.parse(data.name).name}`}/>
+                {open ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
             <Collapse in={open}>
                 <List sx={{width: '100%'}} dense={true} disablePadding>
                     {data.records.map(record => {
+
                         return (
                             <ListItem sx={{pl: 4}} key={record.name} disableGutters><RecordsView
                                 data={data.records}/></ListItem>)
