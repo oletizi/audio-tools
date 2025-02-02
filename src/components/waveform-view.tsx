527c37686303c88bad9a164246ea3ead620e1ab7
orion <oletizi@mac.com>
2025-02-02 10:01:15 -0800

    waveform-view.tsx: * more refactoring

--- src/components/waveform-view.tsx
+++ src/components/waveform-view.tsx
@@ -2,6 +2,24 @@ import {useEffect, useRef, useState} from "react";
 import {Sample} from "@/model/sample";
 import {scale} from "@/lib/lib-core";
 
+interface Rect {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+interface Region extends Rect {
+    isActive: boolean
+}
+
+function isInRect(x: number, y: number, rect: Rect): boolean {
+    return x >= rect.x &&
+        x <= rect.x + rect.width &&
+        y >= rect.y &&
+        y <= rect.y + rect.height;
+}
+
 export function WaveformView({sample, width, height, color = "#aaa", chops}: {
     sample: Sample,
     chops: { start: number, end: number }[]
@@ -9,32 +27,47 @@ export function WaveformView({sample, width, height, color = "#aaa", chops}: {
     return <Waveform sample={sample} width={width} height={height} color={color} chops={chops}/>
 }
 
+
 function Waveform({sample, width, height, color, chops}: { sample: Sample, chops: { start: number, end: number }[] }) {
+    const [regions, setRegions] = useState<Region[]>([])
     const canvasRef = useRef(null)
 
     useEffect(() => {
         const canvas = canvasRef.current
         const ctx = canvas.getContext('2d')
-
-        ctx.fillStyle = 'white'
-        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-
-        ctx.strokeStyle = color
-        ctx.beginPath()
-        ctx.moveTo(0, ctx.canvas.height / 2)
-        ctx.lineTo(ctx.canvas.width, ctx.canvas.height / 2)
-        ctx.stroke()
-        console.log(`Color; ${color}`)
-        console.log(`Sample: channel count: ${sample.getChannelCount()}`)
         const data = sample.getSampleData()
 
         let sum = 0
         let x = 0
         const chunkLength = Math.round(scale(1, 0, ctx.canvas.width, 0, data.length / sample.getChannelCount()))
-        // console.log(`data length: ${data.length}`)
-        // console.log(`per channel length: ${data.length / sample.getChannelCount()}`)
-        // console.log(`chunk length: ${chunkLength}`)
         const mid = ctx.canvas.height / 2
+
+        const chopRegions = []
+
+        if (chops) {
+            for (const c of chops) {
+
+                const startX = scale(c.start, 0, data.length / sample.getChannelCount(), 0, ctx.canvas.width)
+                const endX = scale(c.start, 0, data.length / sample.getChannelCount(), 0, ctx.canvas.width)
+
+                chopRegions.push({x: startX, y: 0, width: endX - startX, height: ctx.canvas.height, isActive: false})
+            }
+            setRegions(chopRegions)
+        }
+
+        // Clear canvas
+        ctx.fillStyle = 'white'
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+        ctx.strokeStyle = color
+        // Draw midline
+        ctx.beginPath()
+        ctx.moveTo(0, mid)
+        ctx.lineTo(ctx.canvas.width, mid)
+        ctx.stroke()
+
+        // Draw waveform
+        ctx.beginPath()
         for (let i = 0; i < data.length; i += sample.getChannelCount()) {
             const datum = Math.abs(data[i])
             sum += datum * datum
@@ -43,12 +76,6 @@ function Waveform({sample, width, height, color, chops}: { sample: Sample, chops
                 const rms = Math.sqrt(sum / chunkLength)
                 const max = Math.pow(2, sample.getBitDepth()) / 2
                 const rmsScaled = Math.round(scale(rms, 0, max, 0, mid))
-                // console.log(`bit depth : ${sample.getBitDepth()}`)
-                // console.log(`max       : ${max}`)
-                // console.log(`rms       :  ${rms}`)
-                // console.log(`rms scaled: ${rmsScaled}`)
-                // console.log(`x         : ${x}`)
-                // console.log(`width     : ${ctx.canvas.width}`)
                 ctx.moveTo(x, mid - rmsScaled)
                 ctx.lineTo(x, mid + rmsScaled)
                 sum = 0
@@ -56,28 +83,36 @@ function Waveform({sample, width, height, color, chops}: { sample: Sample, chops
             }
         }
         ctx.stroke()
-        if (chops) {
-            console.log(`Waveform: Chop count: ${chops.length}`)
-            ctx.strokeStyle = "rgb(25, 118, 210)"
-            for (const c of chops) {
 
-                const startX = scale(c.start, 0, data.length / sample.getChannelCount(), 0, ctx.canvas.width)
-                const endX = scale(c.start, 0, data.length / sample.getChannelCount(), 0, ctx.canvas.width)
-
-                ctx.beginPath()
-                ctx.moveTo(startX, 0)
-                ctx.lineTo(startX, ctx.canvas.height)
-                ctx.stroke()
-
-                ctx.beginPath()
-                ctx.moveTo(endX, 0)
-                ctx.lineTo(endX, ctx.canvas.height)
-                ctx.stroke()
-            }
-        }
+        // Draw chop marks
+        ctx.strokeStyle = "rgb(25, 118, 210)"
+        paintChops(chopRegions, ctx);
 
     }, [sample, chops])
 
-    return <canvas ref={canvasRef} height={height} width={width}/>
+    return <canvas
+        ref={canvasRef}
+        height={height}
+        width={width}
+        onMouseMove={e => {
 
+        }}/>
+}
+
+function paintChops(chopRegions: any[], ctx) {
+    for (const r of chopRegions) {
+        const startX = r.x
+        const endX = r.x + r.width
+        const startY = r.y
+        const endY = r.y + r.height
+        ctx.beginPath()
+        ctx.moveTo(startX, startY)
+        ctx.lineTo(startX, endY)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.moveTo(endX, startY)
+        ctx.lineTo(endX, endY)
+        ctx.stroke()
+    }
 }
